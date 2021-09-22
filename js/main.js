e26c6ca55fa238322bfffad5c2abc12e2dff07b9
mangel45 <mangel.45@hotmail.com>
2021-09-21 23:23:56 -0500

    home scroll

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ for (const link of links) {
 
 function clickHandler(e) {
   e.preventDefault();
-  const href = this.getAttribute("href");
-  console.log(href);  
+  const href = this.getAttribute("href");  
   const offsetTop = document.querySelector(href).offsetTop;  
+
   scroll({
     top: offsetTop,
     behavior: "smooth"
